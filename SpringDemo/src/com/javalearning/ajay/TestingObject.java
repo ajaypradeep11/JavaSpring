a6294d9ffc45109e1bba34b9e9f1be9058b54c61
mpradeep11 <mpradeep11@gmail.com>
2022-12-04 17:39:03 -0500

    Learning 8 - Autowiring by name, type and constructor (Not used often)

--- SpringDemo/src/com/javalearning/ajay/TestingObject.java
+++ SpringDemo/src/com/javalearning/ajay/TestingObject.java
@@ -1,29 +1,50 @@
 package com.javalearning.ajay;
 
-import java.util.List;
-
 public class TestingObject {
 	
-	private List<SmallObjects> objs;
-
+	SmallObjects obj1 = new SmallObjects();
+	SmallObjects obj2 = new SmallObjects();
+	SmallObjects obj3 = new SmallObjects();
 	
 
-	public List<SmallObjects> getObjs() {
-		return objs;
+
+	public SmallObjects getObj1() {
+		return obj1;
 	}
 
 
 
-	public void setObjs(List<SmallObjects> objs) {
-		this.objs = objs;
+	public void setObj1(SmallObjects obj1) {
+		this.obj1 = obj1;
+	}
+
+
+
+	public SmallObjects getObj2() {
+		return obj2;
+	}
+
+
+
+	public void setObj2(SmallObjects obj2) {
+		this.obj2 = obj2;
+	}
+
+
+
+	public SmallObjects getObj3() {
+		return obj3;
+	}
+
+
+
+	public void setObj3(SmallObjects obj3) {
+		this.obj3 = obj3;
 	}
 
 
 
 	public void draw() {
-		for(SmallObjects obj : objs) {
-			System.out.println(obj.value + " got it");
-		}
-		
+		System.out.println(getObj1().value + " " + getObj2().value + " " +getObj3().value);
 	}
 }
